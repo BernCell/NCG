83e64a47d26540950e5ac11dba4f8035bc4c6589
BernCell <bern.cell@hotmail.com>
2024-10-23 00:06:21 +0200

    6eme commit NCG

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
+
   //   console.log("YES !");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
@@ -8,6 +9,8 @@ const menuBtn = document.querySelector(".menu-btn");
 const navElements = document.querySelector(".nav-elements");
 menuBtn.addEventListener("click", (e) => {
   // const close = document.createElement('<i class="fas fa-times"></i>');
-  // menuBtn.classList.toggle(close);
+  const sideBar = document.querySelector(".side-bar");
+  sideBar.classList.toggle("open");
+
   console.log("YES !");
 });
